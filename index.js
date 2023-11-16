f03db2e602e5b5d0469e993ed77b8f449fac918b
QuagKai <quangngokhai@gmail.com>
2023-11-16 19:02:38 +0700

    Fix folder structure

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('homepage/homepage');
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening to port: ${PORT}`)
+});
