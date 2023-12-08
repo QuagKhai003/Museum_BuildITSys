54dff6a3e39e3535393c6a8064e3c30c584f2211
Tienphan09032004 <121601414+Tienphan09032004@users.noreply.github.com>
2023-12-08 23:02:27 +0700

    update

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.render('homepage/homepage');
-})
+});
 
 app.get('/login', (req, res) => {
+    // Render each template separately
     res.render('loginpage/login');
-})
-
-app.get('/login', (req, res) => {
-    res.render('registerpage/register');
-})
+    res.render('registeringpage/register');
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening to port: ${PORT}`)
+    console.log(`Listening to port: ${PORT}`);
 });
