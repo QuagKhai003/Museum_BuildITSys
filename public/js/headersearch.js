5af75a5786b2bf64bde0ec2a1d9a5010c6915479
QuagKai <quangngokhai@gmail.com>
2023-12-11 04:19:21 +0700

    Update header
    - Responsive design for small-screen devices

--- public/js/headersearch.js
+++ public/js/headersearch.js
@@ -1,18 +1,31 @@
+let staticSearchBtn = document.querySelector('.staticSearchBtn');
+let searchBox = document.querySelector('.search');
 let searchbtn = document.querySelector('.searchbtn');
 let closebtn = document.querySelector('.closebtn');
-let searchBox = document.querySelector('.searchBox');
 let searchInput = document.querySelector('input.searchInput')
 
-searchbtn.addEventListener('click', () => {
+staticSearchBtn.addEventListener('click', () => {
+    searchBox.classList.add('open');
     searchbtn.classList.add('open');
     closebtn.classList.add('open');
-    searchBox.classList.add('open');
     searchInput.classList.add('open');
 })
 
 closebtn.addEventListener('click', () => {
+    searchBox.classList.remove('open');
     searchbtn.classList.remove('open');
     closebtn.classList.remove('open');
-    searchBox.classList.remove('open');
     searchInput.classList.remove('open');
+})
+
+let menutoggle = document.querySelector('.menutoggle');
+let closemenubtn = document.querySelector('.closemenubtn');
+let menucontainer = document.querySelector('.menu-container');
+
+menutoggle.addEventListener('click', () => {
+    menucontainer.classList.add('open')
+})
+
+closemenubtn.addEventListener('click', () => {
+    menucontainer.classList.remove('open')
 })
