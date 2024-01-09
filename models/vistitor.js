f74e89d54728834ad59a2a3d531c27d644da4942
Quang Khai <94229717+QuagKhai003@users.noreply.github.com>
2024-01-09 13:18:21 +0700

    Revert "Upload/an"

--- models/vistitor.js
+++ models/vistitor.js
@@ -1,34 +1,20 @@
 const mongoose = require('mongoose');
-const Artist = require('./artist');
 
 const visitorSchema = mongoose.Schema({
-    username: {
+    name: {
         type: String,
-        unique:true,
-        minLength: 6,
-        maxLength: 24,
-        match: /^[a-zA-Z0-9]+$/,
         require: true,
     },
-    email: {
+    gmail: {
         type: String,
-        minLength: 8,
         require: true
     },
     password: {
         type: String,
-        minLength: 8,
         require: true,
     },
     role: {
         type: String,
         require: true,
-        default: 'visitor'
-    },
-    bookmarks: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Savelist'
-    }]
+    }
 })
-
-module.exports = mongoose.model('Visitor', visitorSchema);
