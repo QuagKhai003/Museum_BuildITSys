f571940bce55c38fbc1d81003af6aae2b2da8373
OngMan1 <onggiaman2303@gmail.com>
2024-01-10 23:17:40 +0700

    change password, display artwork for artist, sort pending, approved, declined

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const { checkExistedList } = require('./functions/checkList');
 const { bookmarks } = require('./functions/bookmarks');
 const { uploadArtworks, upload } = require('./functions/uploadArtworks');
 const user = require('./models/user');
+const { changePw } = require('./functions/changePw');
+const { visitorDashboard, artistDashboard, adminDashboard, artworkArtist } = require('./functions/getDashboard');
+const { sortPending, sortApproved, sortDeclined } = require('./functions/sort')
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -20,14 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose.connect(mongoURL)
-.then(() => console.log('Database Connection Sucessfull!'))
-.catch((error) => console.log(error.message));
+    .then(() => console.log('Database Connection Sucessfull!'))
+    .catch((error) => console.log(error.message));
 
 app.use(session({
     secret: 'secret-key-team02-BIT',
     resave: false,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         secure: false, // set false to use the cookie on http not https
         httpOnly: true // for more secure factor
     }
@@ -36,7 +39,7 @@ app.use(session({
 app.get('/', (req, res) => {
     req.session.user
     console.log(req.session.user)
-    res.render('homepage/homepage', {user: req.session.user});
+    res.render('homepage/homepage', { user: req.session.user });
 });
 
 app.get('/login', (req, res) => {
@@ -47,12 +50,12 @@ app.get('/register', (req, res) => {
     res.render('registeringpage/register');
 })
 
-app.post('/register/visitor/', visitorRegister, (req,res) => {
+app.post('/register/visitor/', visitorRegister, (req, res) => {
     console.log(req.session.user)
     console.log("Visitor register route end")
 })
 
-app.post('/register/artist/', artistRegister, async (req,res) => {
+app.post('/register/artist/', artistRegister, async (req, res) => {
     console.log(req.session.user)
     console.log("Artist register route end")
 })
@@ -61,88 +64,86 @@ app.post('/login', authLogin, (req, res) => {
     console.log(req.session.user)
     console.log("Login route end")
 })
-  
+
 app.get('/about', (req, res) => {
     res.render('aboutuspage/aboutus');
 })
 
-app.get('/sidebar' ,(req,res) => {
-    res.render('allartworkpage/sidebar')
-})
 
-app.get('/dashboardTest', (req, res) => {
-    res.render('dashboard/dashboardTest')
-})
+app.get('/dashboardVisitor', visitorDashboard)
+app.get('/dashboardArtist', artistDashboard)
+app.get('/dashboardAdmin', adminDashboard)
 
-app.get('/dashboardVisitor', (req, res) => {
-    res.render('dashboard/profileVisitor', {user: req.session.user})
-})
-
-app.get('/dashboardArtist', (req, res) => {
-    res.render('dashboard/profileArtist', {user: req.session.user})
-})
-app.get('/dashboardAdmin', (req, res) => {
-    res.render('dashboard/profileAdmin', {user: req.session.user})
-})
-
-app.get('/edit' ,(req, res) => {
-res.render('dashboard/edit')
+app.get('/edit', (req, res) => {
+    res.render('dashboard/edit', { user: req.session.user })
 })
 
 
-app.get('/editArtist' ,(req, res) => {
+app.get('/editArtist', (req, res) => {
     res.render('dashboard/editArtist')
 })
 
 
-app.get('/editAdmin' ,(req, res) => {
+app.get('/editAdmin', (req, res) => {
     res.render('dashboard/editAdmin')
 })
 
-app.get('/editArtist' ,(req,res) => {
+app.get('/editArtist', (req, res) => {
     res.render('dashboard/editArtist')
 })
 
 
-app.get('/password' ,(req, res) => {
-res.render('dashboard/password')
+app.get('/password', (req, res) => {
+    req.session.user
+    console.log(req.session.user)
+    if (req.session.user) {
+        res.render('dashboard/password', { user: req.session.user });
+    } else {
+        res.redirect('/error')
+    }
 })
 
-app.get('/passwordArtist' ,(req, res) => {
+app.post('/changePassword', changePw);
+
+app.get('/passwordArtist', (req, res) => {
     res.render('dashboard/passwordArtist')
 })
 
-app.get('/passwordAdmin' ,(req, res) => {
+app.get('/passwordAdmin', (req, res) => {
     res.render('dashboard/passwordAdmin')
 })
 
-app.get('/dashUser', (req, res) => {
-    res.render('dashboardpage/user');
-})
+// app.get('/dashUser', (req, res) => {
+//     res.render('dashboardpage/user');
+// })
 
-app.get('/dashArtist', (req, res) => {
-    res.render('dashboardpage/artist');
-})
+// app.get('/dashArtist', (req, res) => {
+//     res.render('dashboardpage/artist');
+// })
 
-app.get('/dashAdmin', (req, res) => {
-    res.render('dashboardpage/admin');
-})
+// app.get('/dashAdmin', (req, res) => {
+//     res.render('dashboardpage/admin');
+// })
 
-app.get('/dashboard', (req, res) => {
-    res.render('dashboardpage/adminTest.ejs')
-})
+// app.get('/dashboard', (req, res) => {
+//     res.render('dashboardpage/adminTest.ejs')
+// })
+
+app.get('/artworkArtist/pending', sortPending)
+app.get('/artworkArtist/approved', sortApproved)
+app.get('/artworkArtist/declined', sortDeclined)
 
 app.get('/detailedpage', (req, res) => {
     res.render('detailedpage/detailedpage.ejs')
 })
 
 app.get('/error', (req, res) => {
-    res.render('errorpage/errorpage.ejs', {user: req.session.user})
+    res.render('errorpage/errorpage.ejs', { user: req.session.user })
 })
 
-app.get('/artistArtwork', (req, res) => {
-    res.render('dashboard/artworkArtist')
-})
+app.get('/artworkArtist', artworkArtist)
+
+
 
 app.listen(PORT, () => {
     console.log(`Listening to port: ${PORT}`);
@@ -150,23 +151,23 @@ app.listen(PORT, () => {
 
 app.post('/edit-profile', async (req, res) => {
     try {
-      // Assuming you have a form with fields like username, email, etc.
-      const { firstName, lastName, username, email } = req.body;
-  
-      // Assuming you have a visitor's ID (replace 'visitorId' with the actual field name)
-      const userID = req.session.user.id;
-  
-      // Update the visitor's data in the database
-      const updatedVisitor = await user.findByIdAndUpdate(userID, { firstName, lastName, username, email }, { new: true });
-  
-      res.status(200).redirect('/');
+        // Assuming you have a form with fields like username, email, etc.
+        const { firstName, lastName, username, email } = req.body;
+
+        // Assuming you have a visitor's ID (replace 'visitorId' with the actual field name)
+        const userID = req.session.user.id;
+
+        // Update the visitor's data in the database
+        const updatedVisitor = await user.findByIdAndUpdate(userID, { firstName, lastName, username, email }, { new: true });
+
+        res.status(200).redirect('/');
     } catch (error) {
-      console.error('Error while updating profile:', error);
-      res.status(401).redirect("/error")
+        console.error('Error while updating profile:', error);
+        res.status(401).redirect("/error")
     }
 });
 
-app.get('/all', async(req, res) => {
+app.get('/all', async (req, res) => {
     res.render('allpage/allcategories');
 })
 
@@ -192,21 +193,21 @@ app.get('/all', async(req, res) => {
 
 app.get('/detail/:id', async (req, res) => {
     const foundAW = await artworkts.find({ _id: req.params.id })
-    res.render('allpage/detailpage', {foundAW: foundAW , user: req.session.user})
+    res.render('allpage/detailpage', { foundAW: foundAW, user: req.session.user })
 })
 
 app.get('/upload', (req, res) => {
-    res.render('uploadpage/upload', {user: req.session.user});
+    res.render('uploadpage/upload', { user: req.session.user });
 })
 
 app.post('/upload', upload.single('image'), uploadArtworks);
 
-app.get('/logout', async(req, res) => {
-    req.session.destroy((err) =>{
-        if(err) {
-           console.log(err);
-        } else{
+app.get('/logout', async (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        } else {
             res.redirect('/');
         }
-     });
+    });
 })
