856d77797db0363233722d40c86c5b9fc34ce1ad
QuagKai <quangngokhai@gmail.com>
2024-01-17 18:58:50 +0700

    Clean up main branch

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const PORT = 3000;
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
